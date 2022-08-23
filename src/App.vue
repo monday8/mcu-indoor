5a21d8f0410b868cd46113387719d8d50668a3f6
monday8 <66eason66@gmail.com>
2022-08-23 20:30:10 +0800

    new project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './views/Main'
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+}
+</script>
